8d8c992df2ac87a0657846f1d7c80b9d587d2080
usaidaliawuan <147395262+usaidaliawuan@users.noreply.github.com>
2025-05-10 00:59:24 +0500

    config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "DARK-SILENCE-MD~fUITzIzT#ju4vIxCgQuGWAMTocLexk59QdYmsxW_SfZdvQYm1pkw",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
