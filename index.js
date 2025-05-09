6cb4924d1974342d950d251faddf4854543d47d6
usaidaliawuan <147395262+usaidaliawuan@users.noreply.github.com>
2025-05-10 00:56:53 +0500

    index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ const {
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("DARK-SILENCE-MD~", '');
+const sessdata = config.SESSION_ID.replace("DARK-SILENCE-MD~fUITzIzT#ju4vIxCgQuGWAMTocLexk59QdYmsxW_SfZdvQYm1pkw", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
